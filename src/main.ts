cbaadb151955d7aaa0ac2695479d25d0ad879423
francia paucar <134339070+valery-jazminfrancia@users.noreply.github.com>
2023-08-05 19:09:37 -0500

    Add files via upload

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { enableProdMode, importProvidersFrom } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
+
+import { environment } from './environments/environment';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+bootstrapApplication(AppComponent, {
+    providers: [importProvidersFrom(BrowserAnimationsModule, HttpClientModule)]
+})
+  .catch(err => console.error(err));
